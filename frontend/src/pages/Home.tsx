7405ea642291a41f771ed574f3e0e3d9706f4403
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-02-11 16:33:43 +0530

    Added new campaign function

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { IoIosArrowForward } from "react-icons/io";
- 
+
 type Voting = {
   _id?: string,
   category: string;
@@ -24,6 +24,11 @@ type Voting = {
 //   seconds: number;
 // };
 
+type optionInput = {
+  text: string,
+  votes: 0
+}
+
 type User = {
   _id?: string,
   name: string;
@@ -39,9 +44,20 @@ function Home() {
   const [userData, setUserData] = useState<User | null>(null);
   const [votingPopupVisible, setVotingPopupVisible] = useState(false);
   const [submittingVote, setSubmittingVote] = useState(false);
+  const [addingCampaign, setAddingCampaign] = useState(false);
+  const [newCampaignVisible, setNewCampaignVisible] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [input, setInput] = useState('');
-  const [now, setNow] = useState<Date>(new Date())
+  const [now, setNow] = useState<Date>(new Date());
+  const [isActive, setIsActive] = useState(false);
+
+  const [title, setTitle] = useState('');
+  const [source, setSource] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [category, setCategory] = useState('');
+  const [optionNumber, setOptionNumber] = useState(2);
+  const [allCategory, setAllCategory] = useState<string[] | null>(null);
+  const [options, setOptions] = useState<optionInput[]>(Array(optionNumber).fill({ text: "", votes: 0 }));
 
   useEffect(() => {
     const fetchList = async () => {
@@ -138,12 +154,68 @@ function Home() {
     }
   }, [input]);
 
+  const submitCampaign = async () => {
+    if (addingCampaign) return;
+
+    if (!title || !source || !expiry) {
+      toast.error("Empty fields not allowed");
+      return;
+    }
+
+    if (options.length !== optionNumber) {
+      toast.error("Add proper options");
+      return;
+    }
+    setAddingCampaign(true);
+    try {
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/create-campaign`, {
+        title: title.trim(), source: source.trim(), options, expiry, category
+      }, { withCredentials: true });
+
+      if (res.status === 200) {
+        toast.success("Campaign added");
+      }
+
+    } catch (err: any) {
+      if (err && err.response.data.message) {
+        toast.error(err.response.data.message);
+      }
+      else {
+        toast.error("Something went wrong");
+      }
+    }
+    finally {
+      setAddingCampaign(false);
+      setNewCampaignVisible(false);
+      setTitle('');
+      setCategory('');
+      setSource('');
+      setExpiry('');
+    }
+  }
+
+  const handleOptionChange = (index: number, text: string) => {
+    const newOptions = [...options];
+    newOptions[index] = { ...newOptions[index], text };
+    setOptions(newOptions);
+  }
+
+  useEffect(() => {
+    const tempCategory = votingData?.map((item) => {
+      return item.category
+    });
+
+    if (tempCategory) {
+      setAllCategory(tempCategory);
+    }
+  }, [votingData]);
+
   return (
     <>
       <div className={`w-full min-h-screen bg-linear-to-b from-zinc-800 to-zinc-950 flex flex-col justify-start items-center relative overflow-y-auto hide-scrollbar`}>
-        
+
         <Link to='/' className={`w-full cursor-pointer select-none fixed top-0 z-40 text-center py-5 backdrop-blur-3xl bg-white/5 border-b-2 border-b-black text-sm md:text-lg font-light`}>CloudVault</Link>
-        
+
         <h1 className={`w-full text-center mt-24 md:mt-28 text-2xl md:text-4xl font-semibold`}>Ongoing Campaigns</h1>
 
         {/* voting popup */}
@@ -162,12 +234,47 @@ function Home() {
           </div>
         </div>
 
-        <div className={`w-full lg:w-[60%] xl:w-[40%] px-5 mt-7 flex flex-col justify-start items-center`}>
-          <input onChange={(e) => setInput(e.target.value)} placeholder="Search campaign" type="text" className={`w-full py-3 px-3 outline-none text-white text-sm bg-zinc-950`} />
+        {/* new campaign popup */}
+        <div onClick={() => setNewCampaignVisible(false)} className={`w-full ${newCampaignVisible ? "translate-y-0" : "translate-y-full"} duration-500 ease-in-out h-screen fixed top-0 backdrop-blur-2xl bg-black/70 z-40 flex flex-col justify-center items-center`}>
+          <div onClick={(e) => e.stopPropagation()} className={`w-[95%] md:w-[60%] lg:w-[50%] xl:w-[40%] h-auto flex flex-col justify-start items-center`}>
+
+            <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Add a campaign title" type="text" className={`w-[80%] py-3 px-3 outline-none text-white text-sm bg-gray-800`} />
+            <div className={`w-[80%] mt-2 flex justify-start items-center gap-3`}>
+              <span onClick={() => setOptionNumber(2)} className={`w-auto cursor-pointer text-[12px] px-3 py-2 border-2 ${optionNumber === 2 ? "text-white bg-zinc-800 border-blue-600" : "border-transparent bg-zinc-800"}`}>2 options</span>
+              <span onClick={() => setOptionNumber(4)} className={`w-auto cursor-pointer text-[12px] px-3 py-2 border-2 ${optionNumber === 4 ? "text-white bg-zinc-800 border-blue-600" : "border-transparent bg-zinc-800"}`}>4 options</span>
+            </div>
+
+            <div className={`w-[80%] mt-5 flex flex-col justify-start items-center gap-3`}>
+              {
+                Array.from({ length: optionNumber }).map((_, index) => {
+                  return <input onChange={(e) => handleOptionChange(index, e.target.value)} key={index} className={`w-full outline-none text-white text-sm bg-gray-800 px-3 py-2`} placeholder={`Enter option ${index + 1}`} />
+                })
+              }
+            </div>
+            <input value={source} onChange={(e) => setSource(e.target.value)} placeholder="Add a source name" type="text" className={`w-[80%] mt-3 py-3 px-3 outline-none text-white text-sm bg-gray-800`} />
+            <input value={category} onFocus={() => setIsActive(true)} onChange={(e) => setCategory(e.target.value)} type='text' className={`w-[80%] py-2 px-3 bg-gray-800 text-white my-3 outline-none text-start text-sm`} placeholder="Select or add a category" />
+            <div className={`w-[80%] ${isActive ? "block" : "hidden"} flex flex-col justify-start items-start max-h-[30vh] bg-zinc-800 overflow-y-auto hide-scrollbar`}>
+              {
+                isActive && allCategory?.map((item, index) => {
+                  return <span onClick={() => { setCategory(item); setIsActive(false) }} key={index} className={`px-3 py-2 capitalize text-sm cursor-pointer hover:bg-zinc-700 duration-150 ease-in-out`}>{item}</span>
+                })
+              }
+            </div>
+            <div className={`w-[80%] mt-3 flex justify-center items-start h-auto`}>
+              <input onChange={(e) => { setExpiry(e.target.value) }} type='date' className={`w-full bg-gray-800 px-3 py-2`} />
+            </div>
+
+            <p onClick={submitCampaign} className={`w-[80%] text-center mt-5 py-3 bg-linear-to-b from-blue-400 via-blue-500 to-blue-900 flex justify-center items-center gap-2`}>{addingCampaign ? (<>Adding campaign <span className={`loading loading-spinner loading-sm ${addingCampaign ? "block" : "hidden"}`}></span></>) : ("Add campaign")}</p>
+          </div>
         </div>
 
-        <div className={`w-full ${input? "hidden" : "block"} z-30 px-4 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-5`}>
-          {votingData && votingData.map((item, index) => {
+        <div className={`w-full lg:w-[60%] xl:w-[40%] px-5 mt-7 flex flex-col justify-start items-center`}>
+          <input onChange={(e) => setInput(e.target.value)} placeholder="Search campaign" type="text" className={`w-full py-3 px-3 outline-none text-white text-sm bg-zinc-950`} />
+          <p onClick={() => setNewCampaignVisible(true)} className={`select-none w-full mt-2 text-center active:scale-95 duration-150 ease-in-out cursor-pointer text-sm py-2 bg-linear-to-br from-blue-400 via-blue-600 to-blue-500`}>Add campaign</p>
+        </div>
+
+        <div className={`w-full ${input ? "hidden" : "block"} z-30 px-4 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-5`}>
+          {votingData && votingData.slice().reverse().map((item, index) => {
             const left = timeLeft(item.expiry as string);
             return <div key={index} className={`w-full hover:rounded-3xl duration-200 ease-in-out cursor-pointer group flex flex-col justify-start items-start backdrop-blur-md bg-black/30 px-3 py-3`}>
               <p className={`w-auto text-[8px] capitalize px-4 py-1 rounded-full bg-zinc-900`}>{item.category}</p>
